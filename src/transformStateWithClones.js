975b4397da6f5a6aad2025409c97d5376985eb46
Max Hrebinets <brainisbro@Gmail.com>
2024-11-03 13:34:11 +0200

    fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,21 +12,21 @@ function transformStateWithClones(state, actions) {
 
   for (const key of actions) {
     if (key.type === 'addProperties') {
-      for (const i in key.extraData) {
+      for (const i in Object.assign(key.extraData)) {
         StateCopy[i] = key.extraData[i];
       }
       res.push({ ...StateCopy });
     }
 
     if (key.type === 'removeProperties') {
-      for (const i of key.keysToRemove) {
+      for (const i of Object.assign(key.keysToRemove)) {
         delete StateCopy[i];
       }
       res.push({ ...StateCopy });
     }
 
     if (key.type === 'clear') {
-      for (const i in StateCopy) {
+      for (const i in Object.assign(StateCopy)) {
         delete StateCopy[i];
       }
       res.push({ ...StateCopy });
